b1b529f889c72eb261021d5e36a28b632b25d600
Shirish <shirish.chavan@asu.edu>
2017-02-21 20:24:10 -0700

    including operands of type attrDesc

--- src/iterator/Operand.java
+++ src/iterator/Operand.java
@@ -1,10 +1,11 @@
 package iterator;
 
-
+import global.Descriptor;
 
 public class Operand {
   public  FldSpec  symbol;
   public  String   string;
   public  int      integer;
   public  float    real;
+  public Descriptor attrDesc;
 }
