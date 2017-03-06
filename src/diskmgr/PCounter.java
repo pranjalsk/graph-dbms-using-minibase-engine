ea384d25bfd9e62b43e238343c16296630b9898a
Soundarya Manohara <Soundarya Manohara>
2017-03-06 11:33:24 -0700

    2.9- Page Counter

---
+++ src/diskmgr/PCounter.java
@@ -0,0 +1,25 @@
+
+	package diskmgr;
+	
+	public class PCounter {
+	private static int rCounter;
+	private static int wCounter;
+	public static void initialize() {
+	
+	rCounter =0;
+	wCounter =0;
+	}
+	public static void readIncrement() {
+	rCounter++;
+	}
+	public static int getRCounter(){
+		return rCounter;
+	}
+	public static void writeIncrement() {
+	wCounter++;
+	}
+	public static int getWCounter(){
+		return wCounter;
+	}
+	
+	}
