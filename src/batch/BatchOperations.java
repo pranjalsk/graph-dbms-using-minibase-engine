5d874cad12a4e4a032ae31444cece13d97553211
Manjusha Ravindranath <Manjusha Ravindranath>
2017-03-08 16:49:52 -0700

    BatchOperations.java

--- src/batch/BatchOperations.java
+++ src/batch/BatchOperations.java
@@ -1,9 +1,22 @@
 package batch;
 
+import global.Descriptor;
+import heap.HFBufMgrException;
+import heap.HFDiskMgrException;
+import heap.HFException;
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
+
+import nodeheap.NodeHeapfile;
+import diskmgr.DB;
+import diskmgr.DiskMgrException;
+import diskmgr.FileIOException;
+import diskmgr.GraphDB;
+import diskmgr.InvalidPageNumberException;
 
 public class BatchOperations {
 
@@ -14,8 +27,7 @@ public class BatchOperations {
 	private static String nodeHeapFileName ="";
 	
 	private static String graphDBName ="";
-	//= "C:\\Phd_software\\cse510\\test\\Nodefiletest.txt";
-
+ 
 	public static void main(String[] args) {
 
 		BufferedReader br = null;
@@ -47,13 +59,52 @@ public class BatchOperations {
             case 10:
             try {                        	
             	String sCurrentLine;
+            	BatchNodeInsert bi=new BatchNodeInsert();
 				br = new BufferedReader(new FileReader(nodeHeapFileName));       
 				while ((sCurrentLine = br.readLine()) != null) {
 					System.out.println(sCurrentLine);
-					StringTokenizer st=new StringTokenizer(sCurrentLine," ");										
-				}
+					String[] tokens = sCurrentLine.split(" ");
+ 					
+					String label=tokens[0];
+					int value1=Integer.valueOf(tokens[1]);
+					int value2=Integer.valueOf(tokens[2]);
+					int value3=Integer.valueOf(tokens[3]);
+					int value4=Integer.valueOf(tokens[4]);
+					int value5=Integer.valueOf(tokens[5]);
+					Descriptor desc= new Descriptor();
+					desc.set(value1, value2, value3, value4, value5);
+	            
+		            GraphDB dbObject=new GraphDB();
+		            File varTmpDir = new File(graphDBName);
+		            boolean exists = varTmpDir.exists();
+		            if(exists){
+			            dbObject.openDB(graphDBName);
+			            NodeHeapfile nhf=dbObject.createHeapFile(nodeHeapFileName);
+			            bi.insertNode(nhf.get_fileName(), label, desc);
+ 		              }
+		            else
+		            	System.out.println("File do not exists");
+				     }  
               }
 				catch (IOException e) {
+					e.printStackTrace();
+				} catch (InvalidPageNumberException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (FileIOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (DiskMgrException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (HFException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (HFBufMgrException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (HFDiskMgrException e) {
+					// TODO Auto-generated catch block
 					e.printStackTrace();
 				} finally {
 					try {
