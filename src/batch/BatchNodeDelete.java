8cd76a889e5721e6290fdb799c75e98c7200743a
ukeerthi <uthara.keerthi@gmail.com>
2017-03-10 01:15:23 -0700

    Task 12

---
+++ src/batch/BatchNodeDelete.java
@@ -0,0 +1,51 @@
+package batch;
+
+import java.io.*;
+import edgeheap.*;
+import global.*;
+import nodeheap.*;
+import heap.*;
+
+public class BatchNodeDelete {
+
+	public void deleteBatchNode(NodeHeapfile nhf, EdgeHeapFile ehf, String filePath) throws Exception{
+		try{
+			BufferedReader br = new BufferedReader(new FileReader(filePath));
+			String newInput = "";
+			while((newInput = br.readLine()) != null){				
+				String nodeLabel = newInput.trim();
+				BatchInsert batchinsert = new BatchInsert();
+				boolean deleteStatus = false;
+				NID currentNid = new NID(); //To store a copy of the nid
+				
+				//Delete the node				
+				NID newNid = batchinsert.getNidFromNodeLabel(nodeLabel, nhf);
+				currentNid.copyNid(newNid);				
+				deleteStatus = nhf.deleteRecord(newNid);
+				
+				//Delete all edges associated with the node
+				EID newEid = new EID();
+				EScan newEscan = ehf.openScan();
+				Edge newEdge = new Edge();
+				//boolean done = false;
+				NID sourceNID = null;
+				NID destinationNID = null;
+				deleteStatus = false;
+				
+				while((newEdge = newEscan.getNext(newEid)) != null){
+					sourceNID = newEdge.getSource();
+					destinationNID = newEdge.getDestination();
+					if(currentNid.equals(sourceNID) || currentNid.equals(destinationNID)){
+						deleteStatus = ehf.deleteRecord(newEid);
+					}//end-if					
+				}//end-while			
+								
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+
+}
