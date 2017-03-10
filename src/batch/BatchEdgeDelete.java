2c93dce718281bf2beaf0ca23857978d8043dd4a
ukeerthi <uthara.keerthi@gmail.com>
2017-03-10 01:16:01 -0700

    Task 13

---
+++ src/batch/BatchEdgeDelete.java
@@ -0,0 +1,30 @@
+package batch;
+import java.io.*;
+import heap.*;
+import edgeheap.*;
+import nodeheap.*;
+import global.*;
+
+public class BatchEdgeDelete {
+
+	public void deleteBatchEdge(EdgeHeapFile ehf, NodeHeapfile nhf, String filePath) throws Exception{
+		try{
+			BufferedReader br = new BufferedReader(new FileReader(filePath));
+			String newInput = "";
+			while((newInput = br.readLine()) != null){
+				String inputAttributes[] = newInput.trim().split(" ");
+				String sourceLabel = inputAttributes[0];
+				String destinationLabel = inputAttributes[1];
+				String edgeLabel = inputAttributes[2];
+				
+				BatchInsert batchinsert = new BatchInsert();
+				EID newEid = batchinsert.getEidFromEdgeLabel(edgeLabel, ehf);
+				boolean deleteStatus = ehf.deleteRecord(newEid);				
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+}
