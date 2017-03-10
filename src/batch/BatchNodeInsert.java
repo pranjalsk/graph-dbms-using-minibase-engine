01f62cae33d1887b1987df52acb48976c4d452fd
ukeerthi <uthara.keerthi@gmail.com>
2017-03-09 17:04:49 -0700

    Batch operations

--- src/batch/BatchNodeInsert.java
+++ src/batch/BatchNodeInsert.java
@@ -1,50 +1,42 @@
 package batch;
 
-import global.Descriptor;
-import global.NID;
-import heap.HFBufMgrException;
-import heap.HFDiskMgrException;
-import heap.HFException;
-
-import java.io.IOException;
-
-import nodeheap.Node;
-import nodeheap.NodeHeapfile;
+import java.io.*;
+import global.*;
+import nodeheap.*;
+import heap.*;
+import diskmgr.*;
 
 public class BatchNodeInsert {
 
-	 public void insertNode(String filename,String label,Descriptor desc)
-	    {
-		 NodeHeapfile nodeHeapfile = null ;
-		try {
-			nodeHeapfile = new NodeHeapfile(filename);
-			Node node=make_node(label,  desc);
-			NID nodeID=nodeHeapfile.insertNode(node.getNodeByteArray());
-		} catch (HFException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (HFBufMgrException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (HFDiskMgrException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
+	/* Function to read the input file and insert all Nodes
+	 * Input format: <Label Descriptors>
+	 */
+	public void insertBatchNode(NodeHeapfile nhf, FileReader fr) throws Exception{
+		try{
+			BufferedReader br = new BufferedReader(fr);
+			String newInput = "";
+			while((newInput = br.readLine()) != null){
+				String inputAttributes[] = newInput.trim().split(" ");
+				String label = inputAttributes[0];
+				
+				Descriptor desc = new Descriptor();
+				int value0 = Integer.parseInt(inputAttributes[1]);
+				int value1 = Integer.parseInt(inputAttributes[2]);
+				int value2 = Integer.parseInt(inputAttributes[3]);
+				int value3 = Integer.parseInt(inputAttributes[4]);
+				int value4 = Integer.parseInt(inputAttributes[5]);			
+				desc.set(value0, value1, value2, value3, value4);
+				
+				Node newNode = new Node();
+				newNode.setLabel(label);
+				newNode.setDesc(desc);
+				
+				NID newNid = new NID();
+				newNid = nhf.insertNode(newNode.getNodeByteArray()); 
+			}
+		}
+		catch(Exception e){
 			e.printStackTrace();
 		}
-		  
-		}
-
-	   protected Node make_node(String label, Descriptor desc) throws Exception {
-		// TODO Auto-generated method stub
-		Node node =new Node();
-		node.setLabel(label);
-		node.setDesc(desc);
-		return node;
-	  }
-
+	}
 }
--- src/batch/BatchOperations.java
+++ src/batch/BatchOperations.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 import nodeheap.NodeHeapfile;
@@ -19,31 +20,43 @@ import diskmgr.GraphDB;
 import diskmgr.InvalidPageNumberException;
 
 public class BatchOperations {
-
-
 	 
-	private static String taskName ="";
-
-	private static String heapFileName ="";
+	private static String taskName ="";	
+	
+	private static String filePath = "";
 	
 	private static String graphDBName ="";
  
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 
-		BufferedReader br = null;
-		FileReader fr = null;
-		if (args.length < 3)
-	    {
-	        System.out.println("Error: missing argument\n");
-	       // return 1;
-	    }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-			
-			taskName=args[0];
-			heapFileName=args[1];
-			graphDBName=args[2];
-			//fr = new FileReader(heapFileName);
-			//br = new BufferedReader(fr);
+		/* Menu Driven Program (CUI for Batch operations)
+		 * Enter the task name of your choice
+		 * Enter the input file path
+		 * Enter the GraphDB name
+		 * Call the appropriate class methods according to the task number
+		 */
+		System.out.println("\n\nList of Batch Operations");
+		System.out.println("1) batchnodeinsert");
+		System.out.println("2) batchedgeinsert");
+		System.out.println("3) batchnodedelete");
+		System.out.println("4) batchedgedelete");
+		
+		System.out.println("Enter the batch operation, the input file path and the name of the Graph Database in the following format");
+		System.out.println("<task_name> <file_path> <GraphDB_name>");
+		
+		String commandLineInvocation = br.readLine().trim();
+		String inputArguments[] = commandLineInvocation.split(" ");
+		if(inputArguments.length != 3){
+			System.out.println("Error: Invalid input");
+		}
+		
+		else{
+			taskName=inputArguments[0];
+			filePath=inputArguments[1];
+			graphDBName=inputArguments[2];
+	
 			int taskNumber = 0;
 			if(taskName.equalsIgnoreCase("batchnodeinsert"))
 				taskNumber = 10;
@@ -54,90 +67,61 @@ public class BatchOperations {
 			else if(taskName.equalsIgnoreCase("batchedgedelete"))
 				taskNumber = 13;
 			
-            switch(taskNumber){
-            //batch node insert case
-            case 10:
-            try {                        	
-            	String sCurrentLine;
-            	BatchNodeInsert bi=new BatchNodeInsert();
-				br = new BufferedReader(new FileReader(heapFileName));       
-				while ((sCurrentLine = br.readLine()) != null) {
-					System.out.println(sCurrentLine);
-					String[] tokens = sCurrentLine.split(" ");
- 					
-					String label=tokens[0];
-					int value1=Integer.valueOf(tokens[1]);
-					int value2=Integer.valueOf(tokens[2]);
-					int value3=Integer.valueOf(tokens[3]);
-					int value4=Integer.valueOf(tokens[4]);
-					int value5=Integer.valueOf(tokens[5]);
-					Descriptor desc= new Descriptor();
-					desc.set(value1, value2, value3, value4, value5);
-	            
-		            GraphDB dbObject=new GraphDB();
-		            File varTmpDir = new File(graphDBName);
-		            boolean exists = varTmpDir.exists();
-		            if(exists){
-			            dbObject.openDB(graphDBName);
-			            NodeHeapfile nhf=dbObject.createNodeHeapFile(heapFileName);
-			            bi.insertNode(nhf.get_fileName(), label, desc);
- 		              }
-		            else//we need to create a new GraphDB and one NHF and one EHF
-		            	System.out.println("File do not exists");
-				     }  
-              }
-				catch (IOException e) {
+			FileReader fr = new FileReader(filePath);
+	    	GraphDB newGDB = new GraphDB(); //This is to be VERIFIED!!!
+	    	newGDB.openDB(graphDBName);
+	    	
+	        switch(taskNumber){
+	        //Task : Batch node insert
+	        case 10:
+		        try {	        	
+		        	BatchNodeInsert newNodeInsert = new BatchNodeInsert();
+		        	newNodeInsert.insertBatchNode(newGDB.nhf, fr);	        	
+		        }
+				catch (Exception e) {
 					e.printStackTrace();
-				} catch (InvalidPageNumberException e) {
-					// TODO Auto-generated catch block
+				}	        
+	        	break;
+	        
+	        //Task : Batch Edge Insert
+	        case 11:
+	        	try {	        	
+		        	BatchEdgeInsert newEdgeInsert = new BatchEdgeInsert();
+		        	newEdgeInsert.insertBatchEdge(newGDB.ehf, fr);	        	
+		        }
+				catch (Exception e) {
 					e.printStackTrace();
-				} catch (FileIOException e) {
-					// TODO Auto-generated catch block
+				}	    
+	        	break;
+	        	
+	        //Task : Batch Node Delete
+	        case 12:
+	        	try {	        	
+		        	BatchNodeDelete newNodeDelete = new BatchNodeDelete();
+		        	newNodeDelete.deleteBatchNode(newGDB.nhf, fr);	        	
+		        }
+				catch (Exception e) {
 					e.printStackTrace();
-				} catch (DiskMgrException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (HFException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (HFBufMgrException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (HFDiskMgrException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} finally {
-					try {
-						if (br != null)
-							br.close();
-						//if (fr != null)
-						//	fr.close();
-					} catch (IOException ex) {
-						ex.printStackTrace();
-					}
 				}
-            	break;
-            	
-            case 11:
-            	//TODO task 11
-            	break;
-            	
-            case 12:
-            	//TODO task 12
-            	break;
-            	
-            case 13:
-            	//TODO task 13
-            	break;
-            	
-            default:
-            	System.out.println( "Error: unrecognized task number " + taskName);
-                break;
-           
-            }//switch
-		 
-		
+	        	break;
+	        	
+	        //Task : Batch Edge Delete
+	        case 13:
+	        	try {	        	
+		        	BatchEdgeDelete newEdgeDelete = new BatchEdgeDelete();
+		        	newEdgeDelete.deleteBatchEdge(newGDB.ehf, fr);	        	
+		        }
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+	        	break;
+	        	
+	        default:
+	        	System.out.println( "Error: unrecognized task number "+taskName);
+	            break;
+	       
+	        }//switch
+		}//else
 	}//main
-
 
 }
