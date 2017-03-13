aea1548f918ccf6dd8b9ef53c5f1b2b9b8fd1859
sschava1 <shirish.chavan@asu.edu>
2017-03-13 02:43:58 -0700

    conflicts

--- src/batch/BatchNodeInsert.java
+++ src/batch/BatchNodeInsert.java
@@ -17,11 +17,11 @@ public class BatchNodeInsert {
 		try{
 		String[] tokens = sCurrentLine.split(" ");
 		String label=tokens[0];
-		int value1=Integer.valueOf(tokens[1]);
-		int value2=Integer.valueOf(tokens[2]);
-		int value3=Integer.valueOf(tokens[3]);
-		int value4=Integer.valueOf(tokens[4]);
-		int value5=Integer.valueOf(tokens[5]);
+		int value1=Integer.parseInt(tokens[1]);
+		int value2=Integer.parseInt(tokens[2]);
+		int value3=Integer.parseInt(tokens[3]);
+		int value4=Integer.parseInt(tokens[4]);
+		int value5=Integer.parseInt(tokens[5]);
 		Descriptor desc= new Descriptor();
 		desc.set(value1, value2, value3, value4, value5);
 		Node node=make_node(label,  desc);
