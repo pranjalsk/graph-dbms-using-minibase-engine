9b152c4e722ec5b3b6ebd52ea645327bb9fd77b9
Manjusha Ravindranath <Manjusha Ravindranath>
2017-04-16 09:19:52 -0700

    Task 2.6 Path Expression Parsing

--- src/batch/PathExpressionOperations.java
+++ src/batch/PathExpressionOperations.java
@@ -3,13 +3,19 @@ package batch;
 import global.AttrType;
 import global.Descriptor;
 import global.NID;
+import global.TupleOrder;
+import heap.Tuple;
 import iterator.Iterator;
+import iterator.Sort;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
+import nodeheap.Node;
 import diskmgr.GraphDB;
 
 public class PathExpressionOperations {
@@ -33,36 +39,36 @@ public class PathExpressionOperations {
 	static short numbuf = 50;
 
 	public static void parsePathExpression(String commandLineInvocation,
-			String graphDBName, String pathExpressionOperationType)
+			String graphDBName, GraphDB gdb, String pathExpressionOperationType)
 			throws Exception {
 
-		hs = new HashSet<String>();
+		//hs = new HashSet<String>();
 
 		/*
 		 * Menu Driven Program (CUI for Batch operations) Enter the task name of
 		 * your choice Enter the input file path Enter the GraphDB name Call the
 		 * appropriate class methods according to the task number
 		 */
-		System.out.println("commandLineInvocation" + commandLineInvocation);
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("commandLineInvocationVal" + commandLineInvocation);
 
-		if (!hs.contains(graphDBName)) {
+/*		if (!hs.contains(graphDBName)) {
+			System.out.println("Hashset does not contain"+graphDBName);
 			hs.add(graphDBName);
 			GraphDB.initGraphDB(graphDBName);
 			gdb = new GraphDB(0);
 		} else {
 			gdb.openDB(graphDBName);
 		}
-
+*/
 		String inputArguments[] = commandLineInvocation.split("/");
 		int length = inputArguments.length;
-		//System.out.println("Length" + length);
+		System.out.println("Length" + length);
 		attr = new AttrType[length];
 		args = new Object[length];
 		String inputArg = inputArguments[0];
-		//System.out.println("inputArg:" + inputArg);
+		System.out.println("inputArg0:" + inputArg);
 		String inputArg1[] = inputArg.split(" ");
-		//System.out.println("inputArg1.length" + inputArg1.length);
+		System.out.println("inputArg1.length" + inputArg1.length);
 		if (inputArg1.length == 1) {
 			nodeLabelQuery = true;
 			nodeLabel = inputArg;
@@ -95,12 +101,12 @@ public class PathExpressionOperations {
 				|| (pathExpressionOperationType.equalsIgnoreCase("PQ2a"))
 				|| (pathExpressionOperationType.equalsIgnoreCase("PQ3a"))) {
 			for (int i = 1; i < length; i++) {
-				//System.out.println("Input argument:" + inputArguments[i]);
+				System.out.println("Input argument1:" + inputArguments[i]);
 				String inputArgs = inputArguments[i];
 
-				//System.out.println("inputArgs:" + inputArgs);
+				System.out.println("inputArgs:" + inputArgs);
 				String inputArgs1[] = inputArgs.split(" ");
-				//System.out.println("inputArgs1.length" + inputArgs1.length);
+				System.out.println("inputArgs1.length" + inputArgs1.length);
 				if (inputArgs1.length == 1) {
 					nodeLabelQuery = true;
 					nodeLabel = inputArgs;
@@ -111,8 +117,8 @@ public class PathExpressionOperations {
 					nodeDescriptor = inputArgs;
 					attr[i] = new AttrType(5);
 					insideArguments = nodeDescriptor.split(" ");
-					//System.out.println("insideArguments.length"
-						//	+ insideArguments.length);
+					System.out.println("insideArguments.length"
+							+ insideArguments.length);
 					int value1 = Integer.parseInt(insideArguments[0]);
 					int value2 = Integer.parseInt(insideArguments[1]);
 					int value3 = Integer.parseInt(insideArguments[2]);
@@ -125,19 +131,85 @@ public class PathExpressionOperations {
 				}
 
 			}// for
-
+			 Tuple nodesInPath;
+			 Tuple nodesInPathSorted;
+			 Node nextNodeInPath;
+			 String nextNodeInPathLabel;			 
+		    
+		     Set<String> set=new LinkedHashSet<String>();
+			Iterator it=PathExpression.pathExpress1(args, attr,
+					 gdb.nhf.get_fileName(), gdb.ehf.get_fileName(),
+					 gdb.btf_edge_label.get_fileName(), nodeLabel, numbuf,
+					 (short)32);
 			if (pathExpressionOperationType.equalsIgnoreCase("PQ1a")) {
 				// need to make this method static?
-				 Iterator it=PathExpression.pathExpress1(args, attr,
-				 gdb.nhf.get_fileName(), gdb.ehf.get_fileName(),
-				 gdb.btf_edge_label.get_fileName(), nodeLabel, numbuf,
-				 (short)32);
+				 
+				while((nodesInPath=it.get_next())!=null)
+				{
+					nodesInPath.setHdr((short) 1, new AttrType[] { new AttrType(
+							AttrType.attrId) }, new short[] {});
+					
+					NID nextNID=(NID) nodesInPath.getIDFld(1);
+					nextNodeInPath=gdb.nhf.getRecord(nextNID);
+					
+					if(nextNodeInPath != null){
+					nextNodeInPathLabel=nextNodeInPath.getLabel();
+					System.out.println("HeadNodeLabel"+nodeLabel);
+					System.out.println("TailNodeLabel"+nextNodeInPathLabel);
+					}
+					
+				}
+				
 			}
 			if (pathExpressionOperationType.equalsIgnoreCase("PQ2a")) {
 
+				
+				AttrType[] type = new AttrType[1];
+				type[0] = new AttrType(AttrType.attrString);
+				short[] str_sizes=new short[1];
+				str_sizes[0] = (short) 32;
+				Iterator nodesInPathSortedIt = new Sort(type, (short) 2, str_sizes,
+						it, 2, new TupleOrder(0), 32, numbuf);
+				while((nodesInPathSorted=nodesInPathSortedIt.get_next())!=null)
+				{
+					
+					nodesInPathSorted.setHdr((short) 1, new AttrType[] { new AttrType(
+							AttrType.attrId) }, new short[] {});
+					NID nextNID=(NID) nodesInPathSorted.getIDFld(1);
+					nextNodeInPath=gdb.nhf.getRecord(nextNID);
+					
+					if(nextNodeInPath != null){
+					nextNodeInPathLabel=nextNodeInPath.getLabel();
+					System.out.println("HeadNodeLabel"+nodeLabel);
+					System.out.println("TailNodeLabel"+nextNodeInPathLabel);
+					}
+				}
+
 			}
 			if (pathExpressionOperationType.equalsIgnoreCase("PQ3a")) {
-
+				AttrType[] type = new AttrType[1];
+				type[0] = new AttrType(AttrType.attrString);
+				short[] str_sizes=new short[1];
+				str_sizes[0] = (short) 32;
+				Iterator nodesInPathSortedIt = new Sort(type, (short) 2, str_sizes,
+						it, 2, new TupleOrder(0), 32, numbuf);
+				while((nodesInPathSorted=nodesInPathSortedIt.get_next())!=null)
+				{
+					
+					nodesInPathSorted.setHdr((short) 1, new AttrType[] { new AttrType(
+							AttrType.attrId) }, new short[] {});
+					NID nextNID=(NID) nodesInPathSorted.getIDFld(1);
+					nextNodeInPath=gdb.nhf.getRecord(nextNID);
+					if(nextNodeInPath != null){
+					nextNodeInPathLabel=nextNodeInPath.getLabel();
+				    set.add(nextNodeInPathLabel);
+					}
+				}
+				while(set.iterator()!=null)
+				{
+					System.out.println("HeadNodeLabel"+nodeLabel);
+					System.out.println("TailNodeLabel"+set.iterator().next());
+				}
 			}
 		}
 
